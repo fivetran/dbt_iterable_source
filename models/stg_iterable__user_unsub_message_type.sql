5d4195431008e3d284c402fa3d096a6fa01f4cc6
fivetran-reneeli <renee.li@fivetran.com>
2023-11-01 14:55:03 -0400

    try with coalesce

--- models/stg_iterable__user_unsub_message_type.sql
+++ models/stg_iterable__user_unsub_message_type.sql
@@ -34,7 +34,7 @@ final as (
         coalesce(_fivetran_id, email) as unique_user_key,
         cast(message_type_id as {{ dbt.type_string() }} ) as message_type_id,
         {{ dbt_utils.generate_surrogate_key(['_fivetran_id', 'email', 'message_type_id','updated_at']) }} as unsub_message_type_unique_key,
-        rank() over(partition by email order by updated_at desc) as latest_batch_index,
+        rank() over(partition by coalesce(email, _fivetran_id) order by updated_at desc) as latest_batch_index,
         _fivetran_synced
 
     from fields
