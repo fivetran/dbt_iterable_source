6f9ee1a97fc3fa44cc36eaf9ae327207ce018605
Renee Li <91097070+fivetran-reneeli@users.noreply.github.com>
2023-11-01 12:40:08 -0400

    Update macros/get_user_unsubscribed_channel_columns.sql
    Co-authored-by: Joe Markiewicz <74217849+fivetran-joemarkiewicz@users.noreply.github.com>

--- macros/get_user_unsubscribed_channel_columns.sql
+++ macros/get_user_unsubscribed_channel_columns.sql
@@ -1,23 +1,12 @@
 {% macro get_user_unsubscribed_channel_columns() %}
 
-{% if does_table_exist('user_unsubscribed_channel') %}
-
 {% set columns = [
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "_fivetran_id", "datatype": dbt.type_string()},
-    {"name": "channel_id", "datatype": dbt.type_int()}
-] %}
-
-{% else %}
-
-{% set columns = [
-    {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "channel_id", "datatype": dbt.type_int()},
     {"name": "email", "datatype": dbt.type_string()},
     {"name": "updated_at", "datatype": dbt.type_timestamp()}
 ] %}
-
-{% endif %}
 
 {{ return(columns) }}
 
