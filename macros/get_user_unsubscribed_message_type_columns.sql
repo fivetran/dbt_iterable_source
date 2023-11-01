35f44151ce2cfe1de8a1f28f1209a4d311893940
fivetran-reneeli <renee.li@fivetran.com>
2023-11-01 13:36:54 -0400

    fixes

--- macros/get_user_unsubscribed_message_type_columns.sql
+++ macros/get_user_unsubscribed_message_type_columns.sql
@@ -3,7 +3,7 @@
 {% set columns = [
     {"name": "_fivetran_synced", "datatype": dbt.type_timestamp()},
     {"name": "_fivetran_id", "datatype": dbt.type_string()},
-    {"name": "message_type_id", "datatype": dbt.type_string()}
+    {"name": "message_type_id", "datatype": dbt.type_string()},
     {"name": "email", "datatype": dbt.type_string()},
     {"name": "updated_at", "datatype": dbt.type_timestamp()}
 ] %}
