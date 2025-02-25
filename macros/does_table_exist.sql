563f45b580cbdd5f22efa23af27ecd1881d01914
Renee Li <renee.li@fivetran.com>
2025-02-25 15:40:56 -0500

    somehow this is not getting picked up in downstream manifest

--- macros/does_table_exist.sql
+++ macros/does_table_exist.sql
@@ -7,7 +7,7 @@
                 {%- set source_relation = adapter.get_relation(
                         database=node.database,
                         schema=node.schema,
-                        identifier=node.identifier ) -%} 
+                        identifier=node.identifier ) -%}
             {% endif %}
             {%- if source_relation == None -%} 
                 {{ return(False) }} -- return false if relation identified by the database.schema.identifier does not exist for the given table name
